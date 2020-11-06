4a942c67d63b742f814d97f1e63b5220775cccc3
Amaury <amaurycrenier@gmail.com>
2020-11-06 22:50:36 +0100

    contrôle token API

--- api/capteurs/capteur.router.js
+++ api/capteurs/capteur.router.js
@@ -1,9 +1,10 @@
+const { checkToken } = require("../../auth/checkToken");
 const { createCapteur, getCapteursByUtilisateur, updateCapteur, deleteCapteur } = require("./capteur.controller");
 const router = require("express").Router();
 
-router.post("/", createCapteur);
-router.get("/", getCapteursByUtilisateur);
-router.put("/", updateCapteur);
-router.delete("/", deleteCapteur);
+router.post("/", checkToken, createCapteur);
+router.get("/", checkToken, getCapteursByUtilisateur);
+router.put("/", checkToken, updateCapteur);
+router.delete("/", checkToken, deleteCapteur);
 
 module.exports = router;
--- api/utilisateurs/utilisateur.router.js
+++ api/utilisateurs/utilisateur.router.js
@@ -1,9 +1,10 @@
+const { checkToken } = require("../../auth/checkToken");
 const { createUtilisateur, getUtilisateurByEmail, updateUtilisateur, deleteUtilisateur } = require("./utilisateur.controller");
 const router = require("express").Router();
 
-router.post("/", createUtilisateur);
-router.get("/", getUtilisateurByEmail);
-router.put("/", updateUtilisateur);
-router.delete("/", deleteUtilisateur);
+router.post("/", checkToken, createUtilisateur);
+router.get("/", checkToken, getUtilisateurByEmail);
+router.put("/", checkToken, updateUtilisateur);
+router.delete("/", checkToken, deleteUtilisateur);
 
 module.exports = router;
